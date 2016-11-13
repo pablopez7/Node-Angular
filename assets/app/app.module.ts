599e7464585e9bfa9f46c590f130a1765326b99f
pablopez7 <pablopez7@gmail.com>
2016-11-13 16:48:05 -0600

    Routing para Authentication

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -7,18 +7,26 @@ import { AppComponent } from './app.component'
 import { HeaderComponent } from './header.component'
 
 import { AuthenticationComponent } from './auth/authentication.component'
+import { SignupComponent } from './auth/signup.component'
+import { SigninComponent } from './auth/signin.component'
+import { LogoutComponent } from './auth/logout.component'
 
-import { MessageComponent } from "./messages/message.component"
-import { MessagesComponent } from "./messages/messages.component"
-import { MessageListComponent } from "./messages/message-list.component"
-import { MessageInputComponent } from "./messages/message-input.component"
+import { MessageComponent } from './messages/message.component'
+import { MessagesComponent } from './messages/messages.component'
+import { MessageListComponent } from './messages/message-list.component'
+import { MessageInputComponent } from './messages/message-input.component'
 
 @NgModule({
     imports: [ BrowserModule, FormsModule, routing ],
     declarations: [
         AppComponent,
         HeaderComponent,
+
         AuthenticationComponent,
+        SignupComponent,
+        SigninComponent,
+        LogoutComponent,
+
         MessageComponent,
         MessagesComponent,
         MessageListComponent,
---
+++ assets/app/auth/signin.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'signin',
+    templateUrl: './signin.html'
+})
+
+export class SigninComponent { }
